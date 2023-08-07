154e2aeae7050a7d63c30aca8ba7dfa4993616c2
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-08-07 21:52:19 +0200

    converted vanilla code into jQuery
    original vanilla code - commented
    jQuery compiled

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,64 @@
+// vanilla JavaScript code provided
+
+// function newItem() {
+//     let li = document.createElement('li');
+//     let inputValue = document.getElementById('input').value;
+//     let text = document.createTextNode(inputValue);
+//     li.appendChild(text);
+
+//     if (inputValue === '') {
+//         alert('You must write something!');
+//     } else {
+//         let list = document.querySelector('#list');
+//         list.appendChild(li);
+//     }
+
+//     function crossOut() {
+//         li.classList.toggle('strike');
+//     }
+
+//     li.addEventListener('dblclick', crossOut);
+
+//         let crossOutButton = document.createElement('crossOutButton');
+//         crossOutButton.appendChild(document.createTextNode('X'));
+//         li.appendChild(crossOutButton);
+
+//         crossOutButton.addEventListener('click', deleteListItem);
+//         function deleteListItem() {
+//             li.classList.add('delete');
+//         }
+
+//         $('#list').sortable();
+// }
+
+// Converted jQuery code from vanilla 
+
 function newItem() {
-    let li = document.createElement('li');
-    let inputValue = document.getElementById('input').value;
-    let text = document.createTextNode(inputValue);
-    li.appendChild(text);
+
+    let list = $('#list');
+    let inputValue = $('#input').val();
+    let li = $('<li></li>');
+    li.append(inputValue);
 
     if (inputValue === '') {
         alert('You must write something!');
     } else {
-        let list = document.querySelector('#list');
-        list.appendChild(li);
+        list.append(li);
     }
 
-    function crossOut() {
-        li.classList.toggle('strike');
-    }
 
-    li.addEventListener('dblclick', crossOut);
 
-        let crossOutButton = document.createElement('crossOutButton');
-        crossOutButton.appendChild(document.createTextNode('X'));
-        li.appendChild(crossOutButton);
+    li.on('dblclick', function crossOut() {
+        li.toggleClass('strike');
+        });
 
-        crossOutButton.addEventListener('click', deleteListItem);
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+        crossOutButton.on('click', deleteListItem);
         function deleteListItem() {
-            li.classList.add('delete');
+            li.addClass('delete')
         }
-
-        $('#list').sortable();
+    $('#list').sortable();
 }
